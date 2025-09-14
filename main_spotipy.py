5abf4a51a2ca6cd367b453a357cf54749d6815e9
JASNYP <pawel.jasnowski@gmail.com>
2025-09-14 20:29:03 +0200

    spotipy: top_songs by artist + artists by genre + CLI + country code validation

--- main_spotipy.py
+++ main_spotipy.py
@@ -19,9 +19,9 @@ client_secret = os.getenv("CLIENT_SECRET")
 
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
+countries = list(sp.available_markets()['markets'])   # country codes on available on SPOTIFY
 
 # print(client_id, client_secret)
-
 
 #######################################search for artists
 def search_for_artist(artist):
@@ -41,24 +41,32 @@ def search_for_artist(artist):
         print(f'No artist found for " {artist_name} " ')
 
 
-########################## TOP 5 songs in given country
+########################## TOP songs in given country
 
 def top_songs(artist, country):
 
     # SEARCH FOR ARTISTS
+    country = country.upper()
+    if country not in countries:
+        print("Wrong country code")
+        return
+
     artist_name = artist
     results = sp.search(q=f'artist:{artist_name}', type='artist', limit=1)
-    artists = results['artists']['items'][0]
+    artists = results['artists']['items']
+    if not artists:
+        print(f"No artist name: '{artist}' found ... ")
+        return
 
-    if artists:
-        artist_id = artists["id"]
     # SEARCH FOR SONGS
-        top_tracks = sp.artist_top_tracks(artist_id, country)
-        print(f"\nTop 5 most popular tracks of '{artist_name}' in {country} are:")
-        for i, track in enumerate(top_tracks["tracks"][:5], start=1):
-            print(f"{i}. {track['name']} - (popularity: {track['popularity']})")
+    artist_id = artists[0]["id"]
+    top_tracks = sp.artist_top_tracks(artist_id, country)
 
-    else: print(f"No artist name: '{artist}' found ... ")
+    print(f"\nTop 5 most popular tracks of '{artist_name}' in {country} are:")
+    for i, track in enumerate(top_tracks["tracks"][:5], start=1):
+        print(f"{i}. {track['name']} - (popularity: {track['popularity']})")
+
+
 
 ########################## ARTISTS BY GENRE
 
@@ -78,13 +86,13 @@ def artist_in_genre(genre):
     else: print(f"No artists in genre: '{genre}' found ... ")
 
 #################### next action
-
 def next_action():
 
     """Ask user what to do next"""
 
+    input("Press Enter to continue...")
     while True:
-        print("\nWhat do you want to do next?\n")
+        print("\n************ What do you want to do next? ************\n")
         print("1. Back to main menu: \n")
         print("2. EXIT\n")
 
@@ -106,35 +114,36 @@ def next_action():
 # artist_in_genre('blues')
 
 def main():
-    while True:
+    running = True
+    while running:
         print('\n************ SPOTIFY CLI Explorer ************\n')
         print('1. Search by ARTIST\n')
         print('2. Search by TOP SONG of ARTIST in given COUNTRY\n')
         print('3. Search by TOP ARTIST by GENRE\n')
         print('4. EXIT\n')
 
-        choice = input("ENTER number of yout choice:   ").strip()
+        choice = input("ENTER number of your choice: ").strip()
 
         if choice == '1':
             name = input("Enter artist name: ").strip()
             search_for_artist(name)
-            if not next_action():
-                break
+
         elif choice =='2':
             name = input("Enter artist name: ").strip()
-            country = input("Enter coutnry code: " ).strip()
+            country = input("Enter country code (i.e. PL/US/IT/GE) : " ).strip().upper()
             top_songs(name, country)
-            if not next_action():
-                break
+
         elif choice =='3':
             genre = input("Enter genre: ").strip()
             artist_in_genre(genre)
-            if not next_action():
-                break
+
         elif choice == '4':
             print("See you again soon")
             break
+
         else: print("Invalid choice. Try again. \n")
+
+        running = next_action()
 
 
 
